95d3bbb69629d18d9de51df786d5d20e6edd7639
Sude Peker <sudepeker7833@gmail.com>
2023-04-06 01:39:22 +0300

    add commidLine

--- urunler/views.py
+++ urunler/views.py
@@ -1,34 +1,45 @@
-from django.shortcuts import render,redirect
-from .models import*
+from django.shortcuts import render, redirect
+from .models import *
 from django.db.models import Q
 
 # Create your views here.
+
+
 def index(request):
-    return render(request,'anasayfa.html')
+    return render(request, 'index.html')
+
+
 def urunler(request):
-    kategoriler= Kategori.objects.all()
-    urunler=Urun.objects.all()
-    search=''
+    kategoriler = Kategori.objects.all()
+    urunler = Urun.objects.all()
+    search = ''
     if request.GET.get('search'):
-            search=request.GET.get('search')
-            urunler=Urun.objects.filter(
-                Q (isim__icontains = search) |
-                Q (kategori__isim__icontains=search) |
-                Q (sub_category__isim__icontains=search)
-                )
-    context={
-        'kategoriler':kategoriler,
-        'urunler':urunler,
-        'search':search
+        search = request.GET.get('search')
+        urunler = Urun.objects.filter(
+            Q(isim__icontains=search) |
+            Q(kategori__isim__icontains=search) |
+            Q(sub_category__isim__icontains=search)
+        )
+    context = {
+        'kategoriler': kategoriler,
+        'urunler': urunler,
+        'search': search
     }
 
-    return render(request,'urunler.html',context)
-def iletisim(request):
-    return render(request,'iletisim.html')
-def sepet(request):
-    return render(request,'sepet.html')
-def giris(request):
-    return render(request,'giris.html')
+    return render(request, 'urunler.html', context)
 
-def view_404(request,exception):
+
+def iletisim(request):
+    return render(request, 'iletisim.html')
+
+
+def sepet(request):
+    return render(request, 'sepet.html')
+
+
+def giris(request):
+    return render(request, 'giris.html')
+
+
+def view_404(request, exception):
     return redirect('/')
